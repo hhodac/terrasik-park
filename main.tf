625f19e1ae178b4815b7a6ad7de13be7b2225dad
Hai Ho <haiho@Hais-MacBook-Pro.local>
2023-02-19 23:42:28 +1100

    hotfix

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 data "aws_ami" "app_ami" {
-    most_recent = true
-    
-    filter {
-        name   = "name"
-        values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-    }
+  most_recent = true
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
 
-    owners = ["979382823631"] # Bitnami
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
-    ami = data.aws_ami.app_ami.id
-    instance_type = "t2.micro"
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "HelloWorld"
-    }
+  tags = {
+    Name = "HelloWorld"
+  }
 }
